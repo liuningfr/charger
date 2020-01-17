c55bb9a78c1c70a9f5c3f2228e42fd6606c5b03b
ningliu <ningliu@sf-express.com>
2020-01-17 15:05:57 +0800

    完善注释

--- src/Counter/model.js
+++ src/Counter/model.js
@@ -4,6 +4,13 @@ class CounterModel {
   };
 
   increment() {
+    // this.state -> 获取自身 model 的 state
+    // this.setState() -> 更新自身 model 的 state
+    // this.someAction() -> 调用自身 model 的 action
+
+    // this.models.someModel.state -> 获取其它 model 的 state
+    // this.models.someModel.someAction() -> 调用其它 model 的 action
+
     const { count } = this.state;
     this.setState({ count: count + 1 });
   }
@@ -14,6 +21,8 @@ class CounterModel {
   }
 
   async incrementAsync() {
+    // 自动生成的 `someAsyncAction.loading` state 可供使用
+
     await new Promise((resolve) => setTimeout(resolve, 1000));
     this.increment();
   }
