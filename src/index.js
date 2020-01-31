468c4812a1f804221f48c17e0c9ac871a4a9ec11
ningliu <ningliu@sf-express.com>
2020-01-31 20:18:00 +0800

    feat(app): 支持HMR

--- src/index.js
+++ src/index.js
@@ -4,7 +4,17 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const render = (Root) => {
+  ReactDOM.render(<Root />, document.getElementById('root'));
+};
+
+render(App);
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    render(App);
+  });
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
