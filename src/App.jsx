e4b65e9b709ccf136fe313658c3db724a261ba83
ningliu <ning.liu.cn@outlook.com>
2020-02-11 17:29:13 +0800

    feat(app): 更新布局

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'retalk';
-import { Layout } from 'antd';
+import { Layout, Menu, Icon } from 'antd';
 
 import store from '@/store';
 import Demo from '@/pages/Demo';
@@ -8,11 +8,20 @@ import Demo from '@/pages/Demo';
 import './App.scss';
 
 const { Header, Footer, Sider, Content } = Layout;
+const { Item } = Menu;
 
 const App = () => (
   <Provider store={store}>
     <Layout>
-      <Sider>Sider</Sider>
+      <Sider theme="light">
+        <div className="logo">Logo</div>
+        <Menu>
+          <Item key="demo">
+            <Icon type="info-circle" />
+            示例
+          </Item>
+        </Menu>
+      </Sider>
       <Layout>
         <Header>Header</Header>
         <Content>
