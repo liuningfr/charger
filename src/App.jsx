8b32201f0fe08d9a372e11405d1f2706d6afde73
ningliu <ning.liu.cn@outlook.com>
2020-02-11 17:43:58 +0800

    feat(app): 添加react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Provider } from 'retalk';
 import { Layout, Menu, Icon } from 'antd';
+import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
 
 import store from '@/store';
 import Demo from '@/pages/Demo';
@@ -11,26 +12,34 @@ const { Header, Footer, Sider, Content } = Layout;
 const { Item } = Menu;
 
 const App = () => (
-  <Provider store={store}>
-    <Layout>
-      <Sider theme="light">
-        <div className="logo">Logo</div>
-        <Menu>
-          <Item key="demo">
-            <Icon type="info-circle" />
-            示例
-          </Item>
-        </Menu>
-      </Sider>
+  <BrowserRouter>
+    <Provider store={store}>
       <Layout>
-        <Header>Header</Header>
-        <Content>
-          <Demo />
-        </Content>
-        <Footer>Footer</Footer>
+        <Sider theme="light">
+          <div className="logo">Logo</div>
+          <Menu>
+            <Item key="demo">
+              <NavLink to="/demo">
+                <Icon type="info-circle" />
+                示例
+              </NavLink>
+            </Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header>Header</Header>
+          <Content>
+            <Switch>
+              <Route path="/demo">
+                <Demo />
+              </Route>
+            </Switch>
+          </Content>
+          <Footer>Footer</Footer>
+        </Layout>
       </Layout>
-    </Layout>
-  </Provider>
+    </Provider>
+  </BrowserRouter>
 );
 
 export default App;
